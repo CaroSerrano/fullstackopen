ffed3670dd084de40f56f886a3a47ea1e9b2ec70
CaroSerrano <119267971+CaroSerrano@users.noreply.github.com>
2025-02-03 17:45:37 -0300

    countries2

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const Countries = ({ countries }) => {
+const Countries = ({ countries, handleShowBtn }) => {
   if (!countries || countries.length === 0) return null;
 
   if (countries.length > 10) {
@@ -9,9 +9,17 @@ const Countries = ({ countries }) => {
   } else if (countries.length > 1) {
     return (
       <ul>
-        {countries.map((c) => (
-          <li key={c.name.common}>{c.name.common}</li>
-        ))}
+        {countries.map((c) => {
+          return (
+            <div key={c.name.common}>
+              <li>{c.name.common}</li>
+              <button value={c.name.common} onClick={handleShowBtn}>
+                {" "}
+                show
+              </button>
+            </div>
+          );
+        })}
       </ul>
     );
   } else {
@@ -22,29 +30,34 @@ const Countries = ({ countries }) => {
         <p>Capital: {country.capital}</p>
         <p>Area: {country.area}</p>
         <p>Languages:</p>
-        <ul>{Object.entries(country.languages).map(([key, value]) => <li key={key}>{value}</li>)}</ul>
+        <ul>
+          {Object.entries(country.languages).map(([key, value]) => (
+            <li key={key}>{value}</li>
+          ))}
+        </ul>
         <img src={country.flags.png} alt={country.flags.alt} />
       </>
     );
   }
 };
 
-
 function App() {
   const [value, setValue] = useState("");
   const [countries, setCountries] = useState(null);
-  const [filteredCountries, setFilteredCountries] = useState([])
+  const [filteredCountries, setFilteredCountries] = useState([]);
 
   useEffect(() => {
-    axios.get("https://studies.cs.helsinki.fi/restcountries/api/all").then((response) => {
-      setCountries(response.data)
-    });
+    axios
+      .get("https://studies.cs.helsinki.fi/restcountries/api/all")
+      .then((response) => {
+        setCountries(response.data);
+      });
   }, []);
 
   const handleCountry = (event) => {
     const inputValue = event.target.value;
     setValue(inputValue);
-  
+
     if (inputValue.length > 0 && countries) {
       const matches = countries.filter((c) =>
         c.name.common.toLowerCase().includes(inputValue.toLowerCase())
@@ -54,14 +67,23 @@ function App() {
       setFilteredCountries([]);
     }
   };
-  
+
+  const handleShowBtn = (event) => {
+    const countryToShow = countries.filter(
+      (c) => c.name.common.toLowerCase() === event.target.value.toLowerCase()
+    );
+    setFilteredCountries(countryToShow);
+  };
   return (
     <>
       <div>
         find countries <input value={value} onChange={handleCountry} />
       </div>
       <div>
-        <Countries countries= {filteredCountries}/>
+        <Countries
+          countries={filteredCountries}
+          handleShowBtn={handleShowBtn}
+        />
       </div>
     </>
   );
