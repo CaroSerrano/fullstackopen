29a613eba400a39991f9bcd691b6dbbfc4dd2f55
CaroSerrano <119267971+CaroSerrano@users.noreply.github.com>
2025-02-07 13:07:53 -0300

    ex. 4.5

---
+++ part4/blogApp/tests/favoriteBlog.test.js
@@ -0,0 +1,82 @@
+const { test, describe } = require("node:test");
+const assert = require("node:assert");
+const favoriteBlog = require("../utils/list_helper").favoriteBlog;
+
+describe("favorite blog", () => {
+  const listEmpty = [];
+  const listWithOneBlog = [
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+      likes: 5,
+      __v: 0,
+    },
+  ];
+  const listWithMoreBlogs = [
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+      likes: 5,
+      __v: 0,
+    },
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+      likes: 10,
+      __v: 0,
+    },
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+      likes: 12,
+      __v: 0,
+    },
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+      likes: 12,
+      __v: 0,
+    },
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+      likes: 3,
+      __v: 0,
+    },
+  ];
+
+  test("when list has only one blog, equals that", () => {
+    const result = favoriteBlog(listWithOneBlog);
+    assert.deepStrictEqual(result, {
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      likes: 5,
+    });
+  });
+
+  test("when list is empty, returns null", () => {
+    const result = favoriteBlog(listEmpty);
+    assert.strictEqual(result, null);
+  });
+
+  test("of a bigger list is selected rigth", () => {
+    const result = favoriteBlog(listWithMoreBlogs);
+    assert.deepStrictEqual(result, {
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      likes: 12,
+    });
+  });
+});
--- part4/blogApp/utils/list_helper.js
+++ part4/blogApp/utils/list_helper.js
@@ -1,3 +1,5 @@
+const blog = require("../models/blog");
+
 const dummy = (blogs) => {
   return 1;
 };
@@ -5,19 +7,37 @@ const dummy = (blogs) => {
 const totalLikes = (blogs) => {
   if (blogs.length === 0) {
     return 0;
-//   } else if (blogs.length === 1) {
-//     return blogs[0].likes;
   } else {
     const reducer = (sum, item) => {
       return sum + item;
     };
     const likes = blogs.map((b) => b.likes);
     return likes.reduce(reducer, 0);
+  }
+};
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null;
+  } else {
+    let maxLikes = 0;
+    let favoriteBlog;
+    blogs.forEach(blog => {
+        if (blog.likes > maxLikes) {
+            maxLikes = blog.likes
+            favoriteBlog = blog
+        }
+    });
+    return {
+        title: favoriteBlog.title,
+        author: favoriteBlog.author,
+        likes: favoriteBlog.likes
+    }
   }
 };
 
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
