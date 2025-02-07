3eecd3ba6b5a4737873c52b8c871de843bc5e7fb
CaroSerrano <119267971+CaroSerrano@users.noreply.github.com>
2025-02-07 12:26:53 -0300

    refactor: config and .env files

--- part4/blogApp/app.js
+++ part4/blogApp/app.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const config = require("./utils/config");
 const mongoose = require("mongoose");
 const blogRouter = require("./controllers/blogs");
 
-const mongoUrl =
-  "mongodb+srv://fullstackopen:1234@cluster0.unnrajh.mongodb.net/blogApp?retryWrites=true&w=majority&appName=Cluster0";
-mongoose.connect(mongoUrl);
+mongoose.connect(config.MONGODB_URI);
 
 app.use(cors());
 app.use(express.json());
 app.use("/api/blogs", blogRouter);
+
 module.exports = app;
