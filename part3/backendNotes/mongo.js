495a70cba15f036951ad8e49125c30470fdd99bc
CaroSerrano <119267971+CaroSerrano@users.noreply.github.com>
2025-02-10 11:40:45 -0300

    testing noteApp

--- part3/backendNotes/mongo.js
+++ part3/backendNotes/mongo.js
@@ -8,7 +8,7 @@ if (process.argv.length<3) {
 const password = process.argv[2]
 
 const url =
-  `mongodb+srv://fullstackopen:${password}@cluster0.unnrajh.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+  `mongodb+srv://fullstackopen:${password}@cluster0.unnrajh.mongodb.net/testNoteApp?retryWrites=true&w=majority&appName=Cluster0`
 
 mongoose.set('strictQuery',false)
 
@@ -22,18 +22,18 @@ const noteSchema = new mongoose.Schema({
 const Note = mongoose.model('Note', noteSchema)
 
 const note = new Note({
-  content: 'bbbbbb',
+  content: 'Another note',
   important: true,
 })
 
-Note.find({}).then(result => {
-    result.forEach(note => {
-      console.log(note)
-    })
-    mongoose.connection.close()
-  })
+// Note.find({}).then(result => {
+//     result.forEach(note => {
+//       console.log(note)
+//     })
+//     mongoose.connection.close()
+//   })
 
-// note.save().then(result => {
-//   console.log('note saved!')
-//   mongoose.connection.close()
-// })
+note.save().then(result => {
+  console.log('note saved!')
+  mongoose.connection.close()
+})
