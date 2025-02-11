78617b4e05fe0da09807af6b01038e9f3e230557
CaroSerrano <119267971+CaroSerrano@users.noreply.github.com>
2025-02-10 21:15:30 -0300

    ex. 4.15

--- part4/blogApp/app.js
+++ part4/blogApp/app.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const middleware = require("./utils/middleware")
+const middleware = require("./utils/middleware");
 const config = require("./utils/config");
 const mongoose = require("mongoose");
-require('express-async-errors')
+require("express-async-errors");
 const blogRouter = require("./controllers/blogs");
+const userRouter = require("./controllers/users");
 
 mongoose.connect(config.MONGODB_URI);
 
 app.use(cors());
 app.use(express.json());
 app.use("/api/blogs", blogRouter);
-app.use(middleware.errorHandler)
+app.use("/api/users", userRouter);
+app.use(middleware.errorHandler);
 
 module.exports = app;
--- part4/blogApp/tests/test_helper.js
+++ part4/blogApp/tests/test_helper.js
@@ -1,43 +1,51 @@
-const Blog = require('../models/blog')
+const Blog = require("../models/blog");
+const User = require("../models/user");
 
 const initialBlogs = [
   {
     title: "Go To Statement Considered Harmful",
     author: "Edsger W. Dijkstra",
     url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
-    likes: 5
+    likes: 5,
   },
   {
     title: "Go To Statement Considered Harmful",
     author: "aaa",
     url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
-    likes: 10
+    likes: 10,
   },
   {
     title: "Go To Statement Considered Harmful",
     author: "aaa",
     url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
-    likes: 12
+    likes: 12,
   },
   {
     title: "Go To Statement Considered Harmful",
     author: "aaa",
     url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
-    likes: 12
+    likes: 12,
   },
   {
     title: "Go To Statement Considered Harmful",
     author: "aaa",
     url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
-    likes: 3
+    likes: 3,
   },
 ];
 
 const blogsInDb = async () => {
-  const blogs = await Blog.find({})
-  return blogs.map(blog => blog.toJSON())
-}
+  const blogs = await Blog.find({});
+  return blogs.map((blog) => blog.toJSON());
+};
+
+const usersInDb = async () => {
+  const users = await User.find({});
+  return users.map((u) => u.toJSON());
+};
 
 module.exports = {
-  initialBlogs, blogsInDb
-}
+  initialBlogs,
+  blogsInDb,
+  usersInDb,
+};
