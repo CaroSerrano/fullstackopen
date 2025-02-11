27cbcf864a5218702e47a4810276a07c76426118
CaroSerrano <119267971+CaroSerrano@users.noreply.github.com>
2025-02-10 21:19:06 -0300

    add tests for get users

--- part4/blogApp/tests/userApi.test.js
+++ part4/blogApp/tests/userApi.test.js
@@ -3,64 +3,77 @@ const supertest = require("supertest");
 const app = require("../app");
 const helper = require("./test_helper");
 const User = require("../models/user");
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcrypt");
 const assert = require("node:assert");
 
 const api = supertest(app);
 
 describe("when there is initially one user in db", () => {
-    beforeEach(async () => {
-      await User.deleteMany({});
-  
-      const passwordHash = await bcrypt.hash("sekret", 10);
-      const user = new User({ username: "root", passwordHash });
-  
-      await user.save();
-    });
-  
-    test("creation succeeds with a fresh username", async () => {
-      const usersAtStart = await helper.usersInDb();
-  
-      const newUser = {
-        username: "mluukkai",
-        name: "Matti Luukkainen",
-        password: "salainen",
-      };
-  
-      await api
-        .post("/api/users")
-        .send(newUser)
-        .expect(201)
-        .expect("Content-Type", /application\/json/);
-  
-      const usersAtEnd = await helper.usersInDb();
-      assert.strictEqual(usersAtEnd.length, usersAtStart.length + 1);
-  
-      const usernames = usersAtEnd.map((u) => u.username);
-      assert(usernames.includes(newUser.username));
-    });
-    test("creation fails with proper statuscode and message if username already taken", async () => {
-      const usersAtStart = await helper.usersInDb();
-  
-      const newUser = {
-        username: "root",
-        name: "Superuser",
-        password: "salainen",
-      };
-  
-      const result = await api
-        .post("/api/users")
-        .send(newUser)
-        .expect(400)
-        .expect("Content-Type", /application\/json/);
-  
-      const usersAtEnd = await helper.usersInDb();
-      assert(result.body.error.includes("expected `username` to be unique"));
-  
-      assert.strictEqual(usersAtEnd.length, usersAtStart.length);
-    });
+  beforeEach(async () => {
+    await User.deleteMany({});
+
+    const passwordHash = await bcrypt.hash("sekret", 10);
+    const user = new User({ username: "root", passwordHash });
+
+    await user.save();
   });
-  
-  afterAll(async () => {
-    await mongoose.connection.close()
-  })
+
+  test("creation succeeds with a fresh username", async () => {
+    const usersAtStart = await helper.usersInDb();
+
+    const newUser = {
+      username: "mluukkai",
+      name: "Matti Luukkainen",
+      password: "salainen",
+    };
+
+    await api
+      .post("/api/users")
+      .send(newUser)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+
+    const usersAtEnd = await helper.usersInDb();
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length + 1);
+
+    const usernames = usersAtEnd.map((u) => u.username);
+    assert(usernames.includes(newUser.username));
+  });
+  test("creation fails with proper statuscode and message if username already taken", async () => {
+    const usersAtStart = await helper.usersInDb();
+
+    const newUser = {
+      username: "root",
+      name: "Superuser",
+      password: "salainen",
+    };
+
+    const result = await api
+      .post("/api/users")
+      .send(newUser)
+      .expect(400)
+      .expect("Content-Type", /application\/json/);
+
+    const usersAtEnd = await helper.usersInDb();
+    assert(result.body.error.includes("expected `username` to be unique"));
+
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length);
+  });
+
+  test("users are returned as json", async () => {
+    await api
+      .get("/api/users")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  });
+
+  test("all users are returned", async () => {
+    const response = await api.get("/api/users");
+    const usersAtStart = await helper.usersInDb();
+    expect(response.body).toHaveLength(usersAtStart.length);
+  });
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+});
