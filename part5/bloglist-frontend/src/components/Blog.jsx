8c0a691214d8baae9adfb7870ae3a991ef37bc70
CaroSerrano <119267971+CaroSerrano@users.noreply.github.com>
2025-02-17 11:56:49 -0300

    ex. 5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,36 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [detailsVisibility, setDetailsVisibility] = useState(false);
+  console.log(blog);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+  const handleViewBtn = () => {
+    setDetailsVisibility(!detailsVisibility);
+  };
+
+  const detailsStyle = {
+    display: detailsVisibility ? "" : "none",
+  };
+  return (
+    <div style={blogStyle}>
+      {blog.title} <button onClick={handleViewBtn}>view</button>
+      <p>{blog.author} </p>
+      <div style={detailsStyle}>
+        <p>{blog.url}</p>
+        <p>
+          {blog.likes} <button>like</button>
+        </p>
+        {blog.user ? blog.user.name : "User unknown"}
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
