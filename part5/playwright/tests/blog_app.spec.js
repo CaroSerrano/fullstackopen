9cadeaa171b54d8b4905e7de27c9911acfe1b71b
CaroSerrano <119267971+CaroSerrano@users.noreply.github.com>
2025-02-19 17:27:30 -0300

    ex. 5.18

--- part5/playwright/tests/blog_app.spec.js
+++ part5/playwright/tests/blog_app.spec.js
@@ -1,12 +1,33 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test');
+const { loginWith, createBlog } = require('./blogHelper');
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('/api/testing/reset');
+    await request.post('/api/users', {
+      data: {
+        name: 'Caro',
+        username: 'Root',
+        password: '1234',
+      },
+    });
     await page.goto('http://localhost:5173');
   });
 
   test('Login form is shown', async ({ page }) => {
     const locator = await page.getByText('Log in to application');
     await expect(locator).toBeVisible();
+  });
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await loginWith(page, 'Root', '1234')
+      await expect(page.getByText('Caro logged-in')).toBeVisible();
+    });
+
+    test('fails with wrong credentials', async ({ page }) => {
+        await loginWith(page, 'Root', 'wrong')
+        await expect(page.getByText('Caro logged-in')).not.toBeVisible();
+    });
   });
 });
