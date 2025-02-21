c23257818bc3a5053d6be7127cda4ba50a935961
CaroSerrano <119267971+CaroSerrano@users.noreply.github.com>
2025-02-21 13:27:38 -0300

    ex. 6.11

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -3,25 +3,20 @@ import { vote } from '../reducers/anecdoteReducer';
 
 const AnecdoteList = () => {
   const anecdotes = useSelector(({ filter, anecdotes }) => {
-    return anecdotes.filter((a) => a.content.includes(filter));
+    return [...anecdotes].filter((a) => a.content.includes(filter));
   });
   const dispatch = useDispatch();
 
-  const handleVote = (id) => {
-    console.log('vote', id);
-    dispatch(vote(id));
-  };
-
   return (
     <div>
-      {anecdotes
+      {[...anecdotes]
         .sort((a, b) => b.votes - a.votes)
         .map((anecdote) => (
           <div key={anecdote.id}>
             <div>{anecdote.content}</div>
             <div>
               has {anecdote.votes}
-              <button onClick={() => handleVote(anecdote.id)}>vote</button>
+              <button onClick={() => dispatch(vote(anecdote.id))}>vote</button>
             </div>
           </div>
         ))}
