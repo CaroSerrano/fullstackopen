f4abca0f3997178b7a89a3db68bd000742ff8907
CaroSerrano <119267971+CaroSerrano@users.noreply.github.com>
2025-02-24 11:59:45 -0300

    ex. 6.21

--- part6/query-anecdotes/src/App.jsx
+++ part6/query-anecdotes/src/App.jsx
@@ -4,6 +4,7 @@ import { useQuery } from '@tanstack/react-query';
 import { getAnecdotes } from './requests';
 
 const App = () => {
+  
   const result = useQuery({
     queryKey: ['anecdotes'],
     queryFn: getAnecdotes,
