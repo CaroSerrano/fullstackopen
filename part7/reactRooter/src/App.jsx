9f5c590e2ea9a2fd86049595b81c653c6db65667
CaroSerrano <119267971+CaroSerrano@users.noreply.github.com>
2025-02-25 16:57:25 -0300

    part 7d webpack

--- part7/reactRooter/src/App.jsx
+++ part7/reactRooter/src/App.jsx
@@ -1,23 +1,66 @@
 import { useState } from 'react';
-
+import {
+  AppBar,
+  Toolbar,
+  Alert,
+  // Button,
+  Container,
+  TableContainer,
+  Table,
+  TableCell,
+  TableBody,
+  TableRow,
+  Paper,
+  TextField,
+} from '@mui/material';
 import {
   Routes,
   Route,
   Link,
   Navigate,
-  useParams,
   useNavigate,
   useMatch,
 } from 'react-router-dom';
+
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background: Bisque;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid Chocolate;
+  border-radius: 3px;
+`;
+
+const Input = styled.input`
+  margin: 0.25em;
+`;
+
+const Page = styled.div`
+  padding: 1em;
+  background: papayawhip;
+`
+
+const Navigation = styled.div`
+  background: BurlyWood;
+  padding: 1em;
+`
+
+const Footer = styled.div`
+  background: Chocolate;
+  padding: 1em;
+  margin-top: 1em;
+`
 
 const Home = () => (
   <div>
     <h2>TKTL notes app</h2>
     <p>
       Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-      Lorem Ipsum has been the industry's standard dummy text ever since the
-      1500s, when an unknown printer took a galley of type and scrambled it to
-      make a type specimen book. It has survived not only five centuries, but
+      Lorem Ipsum has been the industry&apos;s standard dummy text ever since
+      the 1500s, when an unknown printer took a galley of type and scrambled it
+      to make a type specimen book. It has survived not only five centuries, but
       also the leap into electronic typesetting, remaining essentially
       unchanged. It was popularised in the 1960s with the release of Letraset
       sheets containing Lorem Ipsum passages, and more recently with desktop
@@ -42,13 +85,20 @@ const Note = ({ note }) => {
 const Notes = ({ notes }) => (
   <div>
     <h2>Notes</h2>
-    <ul>
-      {notes.map((note) => (
-        <li key={note.id}>
-          <Link to={`/notes/${note.id}`}>{note.content}</Link>
-        </li>
-      ))}
-    </ul>
+    <TableContainer component={Paper}>
+      <Table>
+        <TableBody>
+          {notes.map((note) => (
+            <TableRow key={note.id}>
+              <TableCell>
+                <Link to={`/notes/${note.id}`}>{note.content}</Link>
+              </TableCell>
+              <TableCell>{note.user}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   </div>
 );
 
@@ -77,18 +127,23 @@ const Login = (props) => {
       <h2>login</h2>
       <form onSubmit={onSubmit}>
         <div>
-          username: <input />
+          username:
+          <Input />
         </div>
         <div>
-          password: <input type='password' />
+          password:
+          <Input type='password' />
         </div>
-        <button type='submit'>login</button>
+        <Button type='submit' primary=''>
+          login
+        </Button>
       </form>
     </div>
   );
 };
 
 const App = () => {
+  // eslint-disable-next-line no-unused-vars
   const [notes, setNotes] = useState([
     {
       id: 1,
@@ -111,9 +166,14 @@ const App = () => {
   ]);
 
   const [user, setUser] = useState(null);
+  const [message, setMessage] = useState(null);
 
   const login = (user) => {
     setUser(user);
+    setMessage(`welcome ${user}`);
+    setTimeout(() => {
+      setMessage(null);
+    }, 10000);
   };
 
   const padding = {
@@ -126,41 +186,29 @@ const App = () => {
     : null;
 
   return (
-    <div>
-      <div>
-        <Link style={padding} to='/'>
-          home
-        </Link>
-        <Link style={padding} to='/notes'>
-          notes
-        </Link>
-        <Link style={padding} to='/users'>
-          users
-        </Link>
-        {user ? (
-          <em>{user} logged in</em>
-        ) : (
-          <Link style={padding} to='/login'>
-            login
-          </Link>
-        )}
-      </div>
+    <Page>
+    <Navigation>
+      <Link style={padding} to="/">home</Link>
+      <Link style={padding} to="/notes">notes</Link>
+      <Link style={padding} to="/users">users</Link>
+      {user
+        ? <em>{user} logged in</em>
+        : <Link style={padding} to="/login">login</Link>
+      }
+    </Navigation>
+    
+    <Routes>
+      <Route path="/notes/:id" element={<Note note={note} />} />  
+      <Route path="/notes" element={<Notes notes={notes} />} />   
+      <Route path="/users" element={user ? <Users /> : <Navigate replace to="/login" />} />
+      <Route path="/login" element={<Login onLogin={login} />} />
+      <Route path="/" element={<Home />} />      
+    </Routes>
 
-      <Routes>
-        <Route path='/notes/:id' element={<Note note={note} />} />
-        <Route path='/notes' element={<Notes notes={notes} />} />
-        <Route
-          path='/users'
-          element={user ? <Users /> : <Navigate replace to='/login' />}
-        />
-        <Route path='/login' element={<Login onLogin={login} />} />
-        <Route path='/' element={<Home />} />
-      </Routes>
-      <footer>
-        <br />
-        <em>Note app, Department of Computer Science 2023</em>
-      </footer>
-    </div>
+    <Footer>
+      <em>Note app, Department of Computer Science 2022</em>
+    </Footer>
+  </Page>
   );
 };
 
