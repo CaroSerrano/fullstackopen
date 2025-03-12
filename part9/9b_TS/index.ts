cba03f19d9911c89784fd40efb1f9e886c32b446
CaroSerrano <119267971+CaroSerrano@users.noreply.github.com>
2025-03-12 11:40:34 -0300

    ex. 9.5

--- part9/9b_TS/index.ts
+++ part9/9b_TS/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import calculateBmi from './bmiCalculator';
 const app = express();
 
 app.get('/ping', (_req, res) => {
@@ -9,6 +10,21 @@ app.get('/hello', (_req, res) => {
   res.send('hello fullstack');
 });
 
+app.get('/bmi', (req, res) => {
+  if (!req.query.height || !req.query.weight) {
+    res.json({ error: 'Provide height and weight parameters' });
+  }
+  const height = req.query.height;
+  const weight = req.query.weight;
+  if (isNaN(Number(height)) || isNaN(Number(weight))) {
+    res.json({ error: 'Query params must be numbers' });
+  }
+  const bmiResult = calculateBmi({
+    height: Number(height),
+    weight: Number(weight),
+  });
+  res.json({ weight: Number(weight), height: Number(height), bmi: bmiResult });
+});
 
 const PORT = 3003;
 
