ff050a124731ad76bd505daa8da3ed763da4503d
CaroSerrano <119267971+CaroSerrano@users.noreply.github.com>
2025-03-14 10:35:11 -0300

    part 9c: ilari type narrowing

--- part9/ilari/src/services/diaryService.ts
+++ part9/ilari/src/services/diaryService.ts
@@ -1,25 +1,42 @@
-import diaries from '../../data/diaries'
-import { DiaryEntry, NonSensitiveDiaryEntry } from '../types';
+import diaries from '../../data/diaries';
+import {
+  DiaryEntry,
+  NonSensitiveDiaryEntry,
+  NewDiaryEntry,
+} from '../types';
+
 
 const getEntries = (): DiaryEntry[] => {
   return diaries;
 };
 
 const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] => {
-    return diaries.map(({ id, date, weather, visibility }) => ({
-        id,
-        date,
-        weather,
-        visibility,
-      }));
+  return diaries.map(({ id, date, weather, visibility }) => ({
+    id,
+    date,
+    weather,
+    visibility,
+  }));
+};
+
+const findById = (id: number): DiaryEntry | undefined => {
+  const entry = diaries.find((d) => d.id === id);
+  return entry;
+};
+
+const addDiary = ( entry: NewDiaryEntry ): DiaryEntry => {
+  const newDiaryEntry = {
+    id: Math.max(...diaries.map(d => d.id)) + 1,
+    ...entry
   };
 
-const addEntry = () => {
-  return null;
+  diaries.push(newDiaryEntry);
+  return newDiaryEntry;
 };
 
 export default {
   getEntries,
   getNonSensitiveEntries,
-  addEntry
+  addDiary,
+  findById,
 };
