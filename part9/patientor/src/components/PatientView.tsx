f65d4cdf3e810201a0b58f4da0996926bfa74ab5
CaroSerrano <119267971+CaroSerrano@users.noreply.github.com>
2025-03-17 15:15:27 -0300

    ex. 9.21

---
+++ part9/patientor/src/components/PatientView.tsx
@@ -0,0 +1,43 @@
+import { Patient } from '../types';
+
+import FemaleIcon from '@mui/icons-material/Female';
+import MaleIcon from '@mui/icons-material/Male';
+import TransgenderIcon from '@mui/icons-material/Transgender';
+
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+
+interface Props {
+  patients: Patient[];
+}
+
+const PatientView = ({ patients }: Props) => {
+  const [errorMessage, setErrorMessage] = useState('');
+  const { id } = useParams();
+  const patient = patients.find((p) => p.id === id);
+  useEffect(() => {
+    if (!patient) {
+      setErrorMessage(`Unable to find patient with id ${id}`);
+      const timer = setTimeout(() => setErrorMessage(''), 5000);
+      return () => clearTimeout(timer);
+    }
+  }, [id, patient]);
+  if (!patient) {
+    return <p>{errorMessage}</p>;
+  }
+
+  return (
+    <div>
+      <h2>
+        {patient.name} {patient.gender === 'female' && <FemaleIcon />}
+        {patient.gender === 'male' && <MaleIcon />}{' '}
+        {patient.gender === 'other' && <TransgenderIcon />}
+      </h2>
+
+      <p>{patient.ssn}</p>
+      <p>{patient.occupation}</p>
+    </div>
+  );
+};
+
+export default PatientView;
